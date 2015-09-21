4e3ec9277317db30c504d68bbf6dd536499294a6
Khanh Dao <knd@Khanhs-MacBook-Pro.local>
2015-09-21 18:31:27 +0700

    [PASSED TEST] Revert to the previous implementation after we have made the test fail

--- src/main/java/com/knd/StringCalculator.java
+++ src/main/java/com/knd/StringCalculator.java
@@ -9,13 +9,8 @@ public class StringCalculator {
             return 0;
         }
         int sum = 0;
-        int counter = 0;
         for (String number : numbers.split(NUMBER_DELIMETER)) {
             sum += Integer.valueOf(number);
-            counter++;
-            if (counter == 3) {
-                break;
-            }
         }
         return sum;
     }
