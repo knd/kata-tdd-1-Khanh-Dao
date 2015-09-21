e1ca570ead90666279f94413a5e6d635cc674a65
Khanh Dao <knd@Khanhs-MacBook-Pro.local>
2015-09-21 17:57:19 +0700

    [REFACTOR] Use the same instance of string calculator in all test methods

--- src/test/java/com/knd/StringCalculatorTest.java
+++ src/test/java/com/knd/StringCalculatorTest.java
@@ -5,24 +5,23 @@ import static org.fest.assertions.api.Assertions.assertThat;
 import org.junit.Test;
 
 public class StringCalculatorTest {
+    
+    StringCalculator calculator = new StringCalculator();
 
     @Test
     public void testCalculatorSummationToZeroForEmptryString() {
-        StringCalculator calculator = new StringCalculator();
         int sum = calculator.add("");
         assertThat(sum).isEqualTo(0);
     }
     
     @Test
     public void testCalculatorSummationForSingleNumberString() {
-        StringCalculator calculator = new StringCalculator();
         int sum = calculator.add("1");
         assertThat(sum).isEqualTo(1);
     }
     
     @Test
     public void testCalculatorSummationForSingleNumberString1() {
-        StringCalculator calculator = new StringCalculator();
         int sum = calculator.add("2");
         assertThat(sum).isEqualTo(2);
     }
