576e52d87da3aec167a063843f831b1dc2cf2134
Khanh Dao <knd@Khanhs-MacBook-Pro.local>
2015-09-21 17:49:18 +0700

    [PASSED TEST] Simplest implementation to pass single number string input

--- src/main/java/com/knd/StringCalculator.java
+++ src/main/java/com/knd/StringCalculator.java
@@ -3,6 +3,9 @@ package com.knd;
 public class StringCalculator {
 
     public int add(String numbers) {
+        if (!numbers.isEmpty()) {
+            return 1;
+        }
         return 0;
     }
 
