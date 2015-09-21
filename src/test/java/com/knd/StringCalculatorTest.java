499bc357601fff8bd93f5a8d588f28fb4c3a8a1c
Khanh Dao <knd@Khanhs-MacBook-Pro.local>
2015-09-21 17:16:33 +0700

    [FAILED TEST] Replace trivial contructor test by compile-error test of add method which accepts empty string

--- src/test/java/com/knd/StringCalculatorTest.java
+++ src/test/java/com/knd/StringCalculatorTest.java
@@ -1,8 +1,13 @@
 package com.knd;
 
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class StringCalculatorTest {
 
-    public void testCalculatorIsInstantiated() {
+    public void testCalculatorAddsUpToZeroForEmptryString() {
         StringCalculator calculator = new StringCalculator();
+        int sum = calculator.add("");
+        assertThat(sum).isEqualTo(0);
     }
+    
 }
