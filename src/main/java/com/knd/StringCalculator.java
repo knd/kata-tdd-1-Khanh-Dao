f543e825e001e9dcafde3c30be639f43651098ab
Khanh Dao <knd@Khanhs-MacBook-Pro.local>
2015-09-21 18:29:42 +0700

    [FAILED TEST] Accidentally had correct implementation from two-number string input scenario, and so the new test is automatically passed. Here we tweak the implementation code to make sure it fails just to test our hypothesis that the amount of numbers actually matter

--- src/main/java/com/knd/StringCalculator.java
+++ src/main/java/com/knd/StringCalculator.java
@@ -9,8 +9,13 @@ public class StringCalculator {
             return 0;
         }
         int sum = 0;
+        int counter = 0;
         for (String number : numbers.split(NUMBER_DELIMETER)) {
             sum += Integer.valueOf(number);
+            counter++;
+            if (counter == 3) {
+                break;
+            }
         }
         return sum;
     }
--- src/test/java/com/knd/StringCalculatorTest.java
+++ src/test/java/com/knd/StringCalculatorTest.java
@@ -2,6 +2,8 @@ package com.knd;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
+import java.util.Random;
+
 import org.junit.Test;
 
 public class StringCalculatorTest {
@@ -27,9 +29,26 @@ public class StringCalculatorTest {
     }
     
     @Test
-    public void testCalculatorSummationForTwoNumbersString() {
+    public void testCalculatorSummationForTwoNumberString() {
         int sum = calculator.add("1,2");
         assertThat(sum).isEqualTo(3);
+    }
+    
+    @Test
+    public void testCalculatorSummationForUnknownAmountOfNumberString() {
+        StringBuffer numbers = new StringBuffer();
+        Random rand = new Random();
+        int amountOfNumbers = rand.nextInt(1000);        // safe enough to avoid hard code
+        int expectedSum = 0;
+        for (int i = 0; i < amountOfNumbers; i++) {
+            expectedSum += i;
+            numbers.append(String.valueOf(i));
+            if (i < amountOfNumbers - 1) {
+                numbers.append(StringCalculator.NUMBER_DELIMETER);
+            }
+        }
+        int sum = calculator.add(numbers.toString());
+        assertThat(sum).isEqualTo(expectedSum);
     }
     
 }
