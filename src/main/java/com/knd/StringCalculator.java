03d1e4ed36ce26417e6c509673b8aca63638cd13
Khanh Dao <knd@Khanhs-MacBook-Pro.local>
2015-09-21 18:13:56 +0700

    [REFACTOR] Use constant comma delimeter

--- src/main/java/com/knd/StringCalculator.java
+++ src/main/java/com/knd/StringCalculator.java
@@ -1,13 +1,15 @@
 package com.knd;
 
 public class StringCalculator {
+    
+    public static final String NUMBER_DELIMETER = ",";
 
     public int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
         }
         int sum = 0;
-        for (String number : numbers.split(",")) {
+        for (String number : numbers.split(NUMBER_DELIMETER)) {
             sum += Integer.valueOf(number);
         }
         return sum;
