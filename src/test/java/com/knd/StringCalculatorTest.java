3c27bf8de406138de06eb2d18951a88bf8c9ba7a
Khanh Dao <knd@Khanhs-MacBook-Pro.local>
2015-09-21 18:37:23 +0700

    [REFACTOR] Refactor test for summation of unknown amount of numbers

--- src/test/java/com/knd/StringCalculatorTest.java
+++ src/test/java/com/knd/StringCalculatorTest.java
@@ -7,7 +7,7 @@ import java.util.Random;
 import org.junit.Test;
 
 public class StringCalculatorTest {
-    
+
     StringCalculator calculator = new StringCalculator();
 
     @Test
@@ -15,40 +15,52 @@ public class StringCalculatorTest {
         int sum = calculator.add("");
         assertThat(sum).isEqualTo(0);
     }
-    
+
     @Test
     public void testCalculatorSummationForSingleNumberString() {
         int sum = calculator.add("1");
         assertThat(sum).isEqualTo(1);
     }
-    
+
     @Test
     public void testCalculatorSummationForSingleNumberString1() {
         int sum = calculator.add("2");
         assertThat(sum).isEqualTo(2);
     }
-    
+
     @Test
     public void testCalculatorSummationForTwoNumberString() {
         int sum = calculator.add("1,2");
         assertThat(sum).isEqualTo(3);
     }
-    
+
     @Test
     public void testCalculatorSummationForUnknownAmountOfNumberString() {
-        StringBuffer numbers = new StringBuffer();
         Random rand = new Random();
-        int amountOfNumbers = rand.nextInt(1000);        // safe enough to avoid hard code
-        int expectedSum = 0;
+        int amountOfNumbers = rand.nextInt(1000); // safe enough to avoid hard code
+        String numbers = getNumbersInputString(amountOfNumbers);
+        int expectedSum = getExpectedSumOFromAmountOfNumbers(amountOfNumbers);
+        int sum = calculator.add(numbers.toString());
+        assertThat(sum).isEqualTo(expectedSum);
+    }
+
+    private String getNumbersInputString(int amountOfNumbers) {
+        StringBuffer numbers = new StringBuffer();
         for (int i = 0; i < amountOfNumbers; i++) {
-            expectedSum += i;
             numbers.append(String.valueOf(i));
             if (i < amountOfNumbers - 1) {
                 numbers.append(StringCalculator.NUMBER_DELIMETER);
             }
         }
-        int sum = calculator.add(numbers.toString());
-        assertThat(sum).isEqualTo(expectedSum);
+        return numbers.toString();
     }
-    
+
+    private int getExpectedSumOFromAmountOfNumbers(int amountOfNumbers) {
+        int expectedSum = 0;
+        for (int i = 0; i < amountOfNumbers; i++) {
+            expectedSum += i;
+        }
+        return expectedSum;
+    }
+
 }
