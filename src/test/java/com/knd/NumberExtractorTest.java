7536f16a1d08e93a5975d50d1f80a956548711bd
Khanh Dao <knd@Khanhs-MacBook-Pro.local>
2015-09-22 10:44:08 +0700

    [FAILED TEST] Add test to returns specified delimiter assuming there is

--- src/test/java/com/knd/NumberExtractorTest.java
+++ src/test/java/com/knd/NumberExtractorTest.java
@@ -17,5 +17,11 @@ public class NumberExtractorTest {
         NumberExtractor extractor = new NumberExtractor("1\n2;3");
         assertThat(extractor.hasSpecifiedDelimiter()).isFalse();
     }
+    
+    @Test
+    public void testExtractorReturnsDelimiterIfNumberStringHasSpecifiedDelimiter() {
+        NumberExtractor extractor = new NumberExtractor("//;\n1;2");
+        assertThat(extractor.getSpecifiedDelimiter()).isEqualTo(";");
+    }
 
 }
