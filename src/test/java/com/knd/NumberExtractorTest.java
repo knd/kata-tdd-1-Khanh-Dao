9898c22b1c574ccd8da24f8830f1308d4d94d4b9
Khanh Dao <knd@Khanhs-MacBook-Pro.local>
2015-09-22 10:56:21 +0700

    [FAILED TEST] Add test to return null when trying to get specified delimiter but there is none

--- src/test/java/com/knd/NumberExtractorTest.java
+++ src/test/java/com/knd/NumberExtractorTest.java
@@ -29,4 +29,10 @@ public class NumberExtractorTest {
         NumberExtractor extractor = new NumberExtractor("//*\n1;2");
         assertThat(extractor.getSpecifiedDelimiter()).isEqualTo("*");
     }
+    
+    @Test
+    public void testExtractorReturnsNullIfNumberStringHasNoSpecifiedDelimiter() {
+        NumberExtractor extractor = new NumberExtractor("1\n2;3");
+        assertThat(extractor.getSpecifiedDelimiter()).isNull();
+    }
 }
