24f01c9e4f4d81cca276ed75a80912ac980eec0d
Khanh Dao <knd@Khanhs-MacBook-Pro.local>
2015-09-22 11:07:30 +0700

    [REFACTOR] Extract constant for readability

--- src/main/java/com/knd/NumberExtractor.java
+++ src/main/java/com/knd/NumberExtractor.java
@@ -4,6 +4,7 @@ public class NumberExtractor {
 
     private static final String DELIMITER_PREFIX = "//";
     private static final int SPECIFIED_DELIMITER_INDEX = 2;
+    private static final String NEW_LINE = "\\n";
 
     private String numbers;
 
@@ -23,7 +24,7 @@ public class NumberExtractor {
     }
 
     public String getNumberString() {
-        return numbers.substring(SPECIFIED_DELIMITER_INDEX + 2);
+        return numbers.substring(SPECIFIED_DELIMITER_INDEX + NEW_LINE.length());
     }
 
 }
