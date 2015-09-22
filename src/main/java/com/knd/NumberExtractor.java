0a4391340e9bb0a451b1e65a5b4ddf3e52b34ca2
Khanh Dao <knd@Khanhs-MacBook-Pro.local>
2015-09-22 10:00:00 +0700

    [PASSED TEST] Implement dummy solution to make test passed

--- src/main/java/com/knd/NumberExtractor.java
+++ src/main/java/com/knd/NumberExtractor.java
@@ -3,7 +3,12 @@ package com.knd;
 public class NumberExtractor {
 
     public NumberExtractor(String numbers) {
-        
+
+    }
+
+    public boolean hasSpecifiedDelimiter() {
+        // TODO Auto-generated method stub
+        return true;
     }
 
 }
