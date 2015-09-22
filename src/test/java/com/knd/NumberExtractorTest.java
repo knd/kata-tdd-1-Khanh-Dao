55b40c81aa0efb6badb225f99f70a1705ebf5a64
Khanh Dao <knd@Khanhs-MacBook-Pro.local>
2015-09-22 09:57:57 +0700

    [FAILED TEST] Add test to check availability of specified delimiter

--- src/test/java/com/knd/NumberExtractorTest.java
+++ src/test/java/com/knd/NumberExtractorTest.java
@@ -1,12 +1,15 @@
 package com.knd;
 
+import static org.fest.assertions.api.Assertions.assertThat;
+
 import org.junit.Test;
 
 public class NumberExtractorTest {
 
     @Test
-    public void testExtractorIsInstantiated() {
-        NumberExtractor extractor = new NumberExtractor("//;\n1;2");       
+    public void testExtractorReturnsTrueIfNumberHasSpecifiedDelimiter() {
+        NumberExtractor extractor = new NumberExtractor("//;\n1;2");
+        assertThat(extractor.hasSpecifiedDelimiter()).isTrue();
     }
 
 }
