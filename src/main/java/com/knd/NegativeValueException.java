a7dfba266fca34f7aab8bd7082e0bfa2ad2e7df2
Khanh Dao <knd@Khanhs-MacBook-Pro.local>
2015-09-22 12:52:37 +0700

    [PASSED TEST] Implement contructor of negative value exception

--- src/main/java/com/knd/NegativeValueException.java
+++ src/main/java/com/knd/NegativeValueException.java
@@ -7,7 +7,19 @@ public class NegativeValueException extends Exception {
     private static final long serialVersionUID = 1L;
     
     public NegativeValueException(List<Integer> negativeValues) {
-        
+        super(getNegativeValueExceptionMessage(negativeValues));
+    }
+
+    private static String getNegativeValueExceptionMessage(List<Integer> negativeValues) {
+        StringBuffer message = new StringBuffer();
+        for (int i = 0; i < negativeValues.size(); i++) {
+            message.append(String.valueOf(negativeValues.get(i)));
+            if (i < negativeValues.size() - 1) {
+                message.append(", ");
+            }
+        }
+        message.append(" are not allowed");
+        return message.toString();
     }
 
 }
