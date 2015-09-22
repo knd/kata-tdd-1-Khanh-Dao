ba7a1e7917729aa4b11e6f832aee8d0afaf2c4ca
Khanh Dao <knd@Khanhs-MacBook-Pro.local>
2015-09-22 11:24:25 +0700

    [PASSED TEST] Implement quirky solution

--- src/main/java/com/knd/StringCalculator.java
+++ src/main/java/com/knd/StringCalculator.java
@@ -6,13 +6,22 @@ public class StringCalculator {
     public static final String NEW_LINE_DELIMETER = "\n";
 
     public int add(String numbers) {
-        if (numbers.isEmpty()) {
-            return 0;
-        }
+        NumberExtractor extractor = new NumberExtractor(numbers);
         int sum = 0;
-        for (String commaSplittedNumber : numbers.split(COMMA_DELIMETER)) {
-            for (String newLineSplittedNumber : commaSplittedNumber.split(NEW_LINE_DELIMETER)) {
-                sum += Integer.valueOf(newLineSplittedNumber);
+        if (extractor.hasSpecifiedDelimiter()) {
+            String delimiter = extractor.getSpecifiedDelimiter();
+            String numberString = extractor.getNumberString();         
+            for (String number : numberString.split(delimiter)) {
+                sum += Integer.valueOf(number);
+            }
+        } else {
+            if (numbers.isEmpty()) {
+                return 0;
+            }
+            for (String commaSplittedNumber : numbers.split(COMMA_DELIMETER)) {
+                for (String newLineSplittedNumber : commaSplittedNumber.split(NEW_LINE_DELIMETER)) {
+                    sum += Integer.valueOf(newLineSplittedNumber);
+                }
             }
         }
         return sum;
